bb7c26aedf7b03fb96c76318d70e8e6635df86d5
Nathanael Labreuil <alabreui@student.42.fr>
2019-10-07 14:05:57 +0200

    Add atoi, strlcpy, strlen and strncmp

---
+++ ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alabreui <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/07 13:34:34 by alabreui          #+#    #+#             */
+/*   Updated: 2019/10/07 14:03:44 by alabreui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	src_size;
+	unsigned int	i;
+
+	src_size = 0;
+	while (src[src_size])
+		src_size++;
+	i = 0;
+	while (i < size - 1 && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	if (size == 0)
+		*dest = '\0';
+	return (src_size);
+}
